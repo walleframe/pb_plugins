24c247802ed1e0a3e02b9934ae0bdfe28c60eb0b
czy <czy463@163.com>
2024-12-15 21:17:59 +0800

    protoc-gen-zerolog

--- cmd/protoc-gen-zerolog/main.go
+++ cmd/protoc-gen-zerolog/main.go
@@ -1,105 +1,283 @@
 package main
 
 import (
-	"github.com/aggronmagi/protokit"
-	"google.golang.org/genproto/googleapis/api/annotations"
-	"google.golang.org/protobuf/proto"
-	"google.golang.org/protobuf/types/pluginpb"
-
-	"bytes"
-	"encoding/json"
 	"fmt"
 	"log"
+	"os"
+	"path/filepath"
+
+	"go.uber.org/multierr"
+	"google.golang.org/protobuf/compiler/protogen"
+	"google.golang.org/protobuf/reflect/protoreflect"
+
+	"github.com/walleframe/pb_plugins/pkg/gen_zerolog"
+)
+
+const genGoDocURL = "https://github.com/walleframe/pb_plugins/blob/main/cmd/protoc-gen-zerolog/readme.org"
+
+var (
+	Version = "0.0.1"
 )
 
 func main() {
-	if err := protokit.RunPlugin(new(plugin)); err != nil {
-		log.Fatal(err)
+	if len(os.Args) == 2 && os.Args[1] == "--version" {
+		fmt.Fprintf(os.Stdout, "%v %v\n", filepath.Base(os.Args[0]), Version)
+		os.Exit(0)
 	}
-}
-
-type plugin struct{}
-
-func (p *plugin) Generate(req *pluginpb.CodeGeneratorRequest) (*pluginpb.CodeGeneratorResponse, error) {
-	descriptors := protokit.ParseCodeGenRequest(req)
-	files := make([]*file, len(descriptors))
-
-	for i, d := range descriptors {
-		files[i] = newFile(d)
+	if len(os.Args) == 2 && os.Args[1] == "--help" {
+		fmt.Fprintf(os.Stdout, "See "+genGoDocURL+" for usage information.\n")
+		os.Exit(0)
 	}
 
-	buf := new(bytes.Buffer)
-	enc := json.NewEncoder(buf)
-	enc.SetIndent("", "  ")
+	// 从环境变量中读取配置
+	gen_zerolog.ParseConfigFromEnv()
 
-	if err := enc.Encode(files); err != nil {
-		return nil, err
-	}
-
-	resp := new(pluginpb.CodeGeneratorResponse)
-	resp.File = append(resp.File, &pluginpb.CodeGeneratorResponse_File{
-		Name:    proto.String("output.json"),
-		Content: proto.String(buf.String()),
-	})
-
-	return resp, nil
-}
-
-type file struct {
-	Name        string     `json:"name"`
-	Description string     `json:"description"`
-	Services    []*service `json:"services"`
-}
-
-func newFile(fd *protokit.FileDescriptor) *file {
-	svcs := make([]*service, len(fd.GetServices()))
-	for i, sd := range fd.GetServices() {
-		svcs[i] = newService(sd)
-	}
-
-	return &file{
-		Name:        fmt.Sprintf("%s.%s", fd.GetPackage(), fd.GetName()),
-		Description: fd.GetComments().String(),
-		Services:    svcs,
-	}
-}
-
-type service struct {
-	Name    string    `json:"name"`
-	Methods []*method `json:"methods"`
-}
-
-func newService(sd *protokit.ServiceDescriptor) *service {
-	methods := make([]*method, len(sd.GetMethods()))
-	for i, md := range sd.GetMethods() {
-		methods[i] = newMethod(md)
-	}
-
-	return &service{Name: sd.GetName(), Methods: methods}
-}
-
-type method struct {
-	Name      string   `json:"name"`
-	HTTPRules []string `json:"http_rules"`
-}
-
-func newMethod(md *protokit.MethodDescriptor) *method {
-	httpRules := make([]string, 0)
-	if httpRule, ok := md.OptionExtensions["google.api.http"].(*annotations.HttpRule); ok {
-		switch httpRule.GetPattern().(type) {
-		case *annotations.HttpRule_Get:
-			httpRules = append(httpRules, fmt.Sprintf("GET %s", httpRule.GetGet()))
-		case *annotations.HttpRule_Put:
-			httpRules = append(httpRules, fmt.Sprintf("PUT %s", httpRule.GetPut()))
-		case *annotations.HttpRule_Post:
-			httpRules = append(httpRules, fmt.Sprintf("POST %s", httpRule.GetPost()))
-		case *annotations.HttpRule_Delete:
-			httpRules = append(httpRules, fmt.Sprintf("DELETE %s", httpRule.GetDelete()))
-		case *annotations.HttpRule_Patch:
-			httpRules = append(httpRules, fmt.Sprintf("PATCH %s", httpRule.GetPatch()))
+	// // 外部命令行参数(比环境变量优先级高)
+	// var flags pflag.FlagSet
+	// cfg := gen_zerolog.Config
+	// _ = cfg
+	// flags.StringVar(&cfg.SvcPkg, "pkg_svc", cfg.SvcPkg, "service package name")
+	protogen.Options{
+		//ParamFunc: flags.Set,
+	}.Run(func(gen *protogen.Plugin) (err error) {
+		err = gen_zerolog.InitTemplate()
+		if err != nil {
+			log.Println("gen_mysql InitTemplate error: ", err)
+			return
 		}
-		// Append more for each rule in httpRule.AdditionalBindings...
-	}
 
-	return &method{Name: md.GetName(), HTTPRules: httpRules}
+		for _, f := range gen.Files {
+			if !f.Generate {
+				continue
+			}
+			err = genZerologFile(gen, f)
+			if err != nil {
+				return err
+			}
+		}
+		//gen.SupportedFeatures = uint64(pluginpb.CodeGeneratorResponse_FEATURE_PROTO3_OPTIONAL)
+		return
+	})
+}
+
+func genZerologFile(gen *protogen.Plugin, file *protogen.File) (err error) {
+	filename := file.GeneratedFilenamePrefix + ".zlog.go"
+
+	zlog := &gen_zerolog.ZlogMessages{
+		File: filename,
+	}
+	zlog.ToolName = "protoc-gen-zerolog"
+	zlog.Package = string(file.GoPackageName)
+	// 扫描要生成的消息
+	genZerologObject(file, zlog)
+	// generate
+	data, err := gen_zerolog.Generate(zlog)
+	if err != nil {
+		err = multierr.Append(err, fmt.Errorf("generate zerolog file %s error: %v", filename, err))
+		return
+	}
+	// write response
+	for _, d := range data {
+		file := gen.NewGeneratedFile(d.File, "")
+		file.Write(d.Data)
+	}
+	return
+}
+
+func genZerologObject(file *protogen.File, zlog *gen_zerolog.ZlogMessages) {
+	for _, m := range file.Messages {
+		genZerologMessage(m, zlog)
+
+	}
+}
+
+func genZerologMessage(m *protogen.Message, zlog *gen_zerolog.ZlogMessages) {
+	if m.Desc.IsMapEntry() {
+		return
+	}
+	msg := &gen_zerolog.Message{
+		Name: m.GoIdent.GoName,
+		Type: string(m.GoIdent.GoName),
+	}
+	for _, f := range m.Fields {
+		genZerologField(f, msg, zlog)
+	}
+	zlog.Msgs = append(zlog.Msgs, msg)
+
+	for _, sub := range m.Messages {
+		genZerologMessage(sub, zlog)
+	}
+}
+
+func genZerologField(f *protogen.Field, msg *gen_zerolog.Message, zlog *gen_zerolog.ZlogMessages) {
+	if f.Desc.IsWeak() {
+		return
+	}
+	field := &gen_zerolog.Field{
+		Key:  string(f.Desc.Name()),
+		Name: f.GoName,
+	}
+	field.Value = func(obj string) string {
+		return fmt.Sprintf("%s.%s", obj, field.Name)
+	}
+	if f.Desc.IsMap() {
+		field.Func = "Object"
+		field.Map = &gen_zerolog.MapField{}
+		// Type: string(f.Desc.MapKey().Name()),
+		//KeyFunc: getFieldFunc(f.Message.Fields[0]),
+		field.Map.Type = getMapFieldType(f)
+
+		field.Map.KeyFunc = getMapKeyFunc(f.Message.Fields[0], zlog)
+
+		field.Map.ValFunc = getFieldFunc(f.Message.Fields[1], field, zlog)
+	} else {
+		field.Func = getFieldFunc(f, field, zlog)
+	}
+	msg.Fields = append(msg.Fields, field)
+}
+
+func getMapFieldType(f *protogen.Field) string {
+	key := f.Message.Fields[0]
+	value := f.Message.Fields[1]
+	typ := "map[" + key.Desc.Kind().String() + "]"
+	if value.Desc.Kind() == protoreflect.MessageKind {
+		if value.Message.GoIdent.GoImportPath == f.Parent.GoIdent.GoImportPath {
+			typ += "*" + value.Message.GoIdent.GoName
+		} else {
+			typ += "*" + filepath.Base(string(value.Message.GoIdent.GoImportPath)) + "." + value.Message.GoIdent.GoName
+		}
+	} else {
+		typ += value.Desc.Kind().String()
+	}
+	return typ
+}
+
+func getMapKeyFunc(f *protogen.Field, zlog *gen_zerolog.ZlogMessages) func(k string) string {
+
+	if f.Desc.Kind() != protoreflect.StringKind {
+		zlog.Import("strconv", "FormatInt/FormatUint")
+	}
+	switch f.Desc.Kind() {
+	case protoreflect.StringKind:
+		return func(k string) string {
+			return k
+		}
+	case protoreflect.EnumKind:
+		return func(k string) string {
+			return fmt.Sprintf("%s.String()", k)
+		}
+	case protoreflect.Int32Kind, protoreflect.Sint32Kind, protoreflect.Sfixed32Kind:
+		return func(k string) string {
+			return fmt.Sprintf("strconv.FormatInt(int64(%s), 10)", k)
+		}
+	case protoreflect.Uint32Kind, protoreflect.Fixed32Kind:
+		return func(k string) string {
+			return fmt.Sprintf("strconv.FormatUint(uint64(%s), 10)", k)
+		}
+	case protoreflect.Int64Kind, protoreflect.Sint64Kind, protoreflect.Sfixed64Kind:
+		return func(k string) string {
+			return fmt.Sprintf("strconv.FormatInt(%s, 10)", k)
+		}
+	case protoreflect.Uint64Kind, protoreflect.Fixed64Kind:
+		return func(k string) string {
+			return fmt.Sprintf("strconv.FormatUint(%s, 10)", k)
+		}
+	case protoreflect.FloatKind, protoreflect.DoubleKind, protoreflect.BytesKind, protoreflect.MessageKind, protoreflect.GroupKind, protoreflect.BoolKind:
+		// protobuf 不支持这些类型作为map的key
+		return nil
+	}
+	return nil
+}
+
+func getFieldFunc(f *protogen.Field, fcfg *gen_zerolog.Field, zlog *gen_zerolog.ZlogMessages) string {
+	if fcfg.Map != nil {
+		fcfg.Value = func(obj string) string {
+			return obj
+		}
+	}
+	if f.Desc.IsList() {
+		switch f.Desc.Kind() {
+		case protoreflect.StringKind:
+			return "Strs"
+		case protoreflect.BytesKind:
+			fcfg.Value = func(obj string) string {
+				zlog.Import("encoding/base64", "EncodeToString")
+				return fmt.Sprintf(`func()(vals []string){
+	vals = make([]string, 0, len(%[1]s.%[2]s))
+	for _,v := range %[1]s.%[2]s {
+		vals = append(vals, base64.StdEncoding.EncodeToString(v))
+	}
+	return vals
+}()`, obj, fcfg.Name)
+			}
+			return "Strs"
+		case protoreflect.BoolKind:
+			return "Bools"
+		case protoreflect.EnumKind:
+			fcfg.Value = func(obj string) string {
+				return fmt.Sprintf(`func()(vals []string){
+	vals = make([]string, 0, len(%[1]s.%[2]s))
+	for _,v := range %[1]s.%[2]s {
+		vals = append(vals, v.String())
+	}
+	return vals
+}()`, obj, fcfg.Name)
+			}
+			return "Strs"
+		case protoreflect.MessageKind, protoreflect.GroupKind:
+			fcfg.Value = func(obj string) string {
+				return fmt.Sprintf(`%[3]sArray(%[1]s.%[2]s)`, obj, fcfg.Name, f.Message.GoIdent.GoName)
+			}
+			return "Array"
+		case protoreflect.Int32Kind, protoreflect.Sint32Kind, protoreflect.Sfixed32Kind:
+			return "Ints32"
+		case protoreflect.Uint32Kind, protoreflect.Fixed32Kind:
+			return "Uints32"
+		case protoreflect.Int64Kind, protoreflect.Sint64Kind, protoreflect.Sfixed64Kind:
+			return "Ints64"
+		case protoreflect.Uint64Kind, protoreflect.Fixed64Kind:
+			return "Uints64"
+		case protoreflect.FloatKind:
+			return "Floats32"
+		case protoreflect.DoubleKind:
+			return "Floats64"
+		}
+		return ""
+	}
+	switch f.Desc.Kind() {
+	case protoreflect.StringKind:
+		return "Str"
+	case protoreflect.BytesKind:
+		if fcfg.Map != nil {
+			fcfg.Value = func(obj string) string {
+				zlog.Import("encoding/base64", "EncodeToString")
+				return fmt.Sprintf("base64.StdEncoding.EncodeToString(%s)", obj)
+			}
+		} else {
+			fcfg.Value = func(obj string) string {
+				zlog.Import("encoding/base64", "EncodeToString")
+				return fmt.Sprintf("base64.StdEncoding.EncodeToString(%s.%s)", obj, fcfg.Name)
+			}
+		}
+		return "Str"
+	case protoreflect.BoolKind:
+		return "Bool"
+	case protoreflect.EnumKind:
+		return "Stringer"
+	case protoreflect.MessageKind, protoreflect.GroupKind:
+		return "Object"
+	case protoreflect.Int32Kind, protoreflect.Sint32Kind, protoreflect.Sfixed32Kind:
+		return "Int32"
+	case protoreflect.Uint32Kind, protoreflect.Fixed32Kind:
+		return "Uint32"
+	case protoreflect.Int64Kind, protoreflect.Sint64Kind, protoreflect.Sfixed64Kind:
+		return "Int64"
+	case protoreflect.Uint64Kind, protoreflect.Fixed64Kind:
+		return "Uint64"
+	case protoreflect.FloatKind:
+		return "Float32"
+	case protoreflect.DoubleKind:
+		return "Float64"
+	}
+	return ""
 }
--- pkg/gen_zerolog/gen_zerolog.go
+++ pkg/gen_zerolog/gen_zerolog.go
@@ -15,43 +15,39 @@ package gen_zerolog
 
 import (
 	_ "embed"
-	"strings"
-
-	"path/filepath"
 
 	"github.com/walleframe/pb_plugins/pkg/tpl"
-	"github.com/walleframe/pb_plugins/pkg/utils"
 )
 
 //go:embed zerolog.tpl
 var mainTpl string
 
 type GenerateEnv struct {
-	SvcPkg  string
-	UtilPkg string
-	Charset string
-	Collate string
+	// SvcPkg  string
+	// UtilPkg string
+	// Charset string
+	// Collate string
 }
 
 var Config = &GenerateEnv{
-	SvcPkg:  "github.com/walleframe/svc_db",
-	UtilPkg: "github.com/walleframe/walle/util",
-	Charset: "utf8mb4",
-	Collate: "utf8mb4_general_ci",
+	// SvcPkg:  "github.com/walleframe/svc_db",
+	// UtilPkg: "github.com/walleframe/walle/util",
+	// Charset: "utf8mb4",
+	// Collate: "utf8mb4_general_ci",
 }
 
 func ParseConfigFromEnv() {
-	// 如果环境变量设置了值, 读取作为为默认值. 优先使用传递的参数
-	utils.GetEnvString("ZAP_COLLATE", &Config.Collate)
-	utils.GetEnvString("ZAP_CHARSET", &Config.Charset)
-	utils.GetEnvString("ZAP_UTIL_PKG", &Config.UtilPkg)
-	utils.GetEnvString("ZAP_SVC_PKG", &Config.SvcPkg)
+	// // 如果环境变量设置了值, 读取作为为默认值. 优先使用传递的参数
+	// utils.GetEnvString("ZAP_COLLATE", &Config.Collate)
+	// utils.GetEnvString("ZAP_CHARSET", &Config.Charset)
+	// utils.GetEnvString("ZAP_UTIL_PKG", &Config.UtilPkg)
+	// utils.GetEnvString("ZAP_SVC_PKG", &Config.SvcPkg)
 }
 
 var gen *tpl.GoTemplate
 
 func InitTemplate() (err error) {
-	gen = tpl.NewTemplate("zap")
+	gen = tpl.NewTemplate("zerolog")
 	gen.Funcs(map[string]interface{}{})
 	err = gen.Parse(mainTpl)
 	if err != nil {
@@ -61,14 +57,37 @@ func InitTemplate() (err error) {
 	return nil
 }
 
-type ZapMessages struct {
-	tpl.GoObject
-
-	File string
+type MapField struct {
+	Type    string                // go里面,map的类型
+	KeyFunc func(k string) string // key的函数
+	ValFunc string                // value的函数
 }
 
-func Generate(tbl *ZapMessages) (out []*tpl.BuildOutput, err error) {
-	tbl.Version = "0.0.1"
+type Field struct {
+	Key   string // 写入日志的字段名
+	Name  string // go结构体字段名
+	Func  string // 使用的函数方法
+	Value func(obj string) string
+	Map   *MapField // 是否是map
+}
+
+type Message struct {
+	Name   string
+	Type   string
+	Fields []*Field
+}
+
+type ZlogMessages struct {
+	tpl.GoObject
+
+	Package string
+	File    string
+
+	Msgs []*Message
+}
+
+func Generate(zlog *ZlogMessages) (out []*tpl.BuildOutput, err error) {
+	zlog.Version = "0.0.1"
 
 	// genTpl, err := gen.Clone()
 	// if err != nil {
@@ -76,29 +95,23 @@ func Generate(tbl *ZapMessages) (out []*tpl.BuildOutput, err error) {
 	// }
 	genTpl := gen
 
-	genTpl.AddImportFunc(tbl)
+	genTpl.AddImportFunc(zlog)
 
 	for _, pkg := range []string{
-		"context",
-		"database/sql",
-		"errors",
-		"strings",
-		"fmt",
-		"sync/atomic",
-		"github.com/jmoiron/sqlx",
+		"github.com/rs/zerolog",
 	} {
-		tbl.Import(pkg, "pkg")
+		zlog.Import(pkg, "pkg")
 	}
-	tbl.Import(Config.SvcPkg, "svc_db")
-	tbl.Import(Config.UtilPkg, "util")
+	// tbl.Import(Config.SvcPkg, "svc_db")
+	// tbl.Import(Config.UtilPkg, "util")
 
-	data, err := genTpl.Exec(tbl)
+	data, err := genTpl.Exec(zlog)
 	if err != nil {
 		return nil, err
 	}
 
 	out = append(out, &tpl.BuildOutput{
-		File: filepath.Join(strings.TrimSuffix(tbl.File, filepath.Ext(tbl.File)), ".zap.go"),
+		File: zlog.File,
 		Data: data,
 	})
 
